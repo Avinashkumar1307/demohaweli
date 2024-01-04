e351b238d5aaedb3d77a2c5362b9aafb565ee872
Avinash kumar <avinash2y20@gmail.com>
2024-01-04 22:48:56 +0530

    pushing

---
+++ BackEnd/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+require('./config/database').connect();
+
+// Routes import and mount
+
+const user = require('./routes/user');
+app.use('/api/v1', user);
+const phonepeRoute = require('./routes/phonepeRoute')
+app.use("/api", phonepeRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is Started ${PORT}`);
+})
