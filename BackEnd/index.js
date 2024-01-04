fa2960c85f201cf6bc95f3e3277a3b242aceabfb
Avinash kumar <121241717+Avinashkumar1307@users.noreply.github.com>
2024-01-04 23:19:16 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://demohaweli-frontend.vercel.app",
+        origin: ["https://demohaweli-frontend.vercel.app"],
         methods:["POST","GET"],
         credentials:true
     })
@@ -17,9 +17,9 @@ require('./config/database').connect();
 // Routes import and mount
 
 const user = require('./routes/user');
-app.use('/api/v1', user);
+// app.use('/api/v1', user);
 const phonepeRoute = require('./routes/phonepeRoute')
-app.use("/api", phonepeRoute);
+// app.use("/api", phonepeRoute);
 
 app.get("/",(req,res) =>{
     res.json("hello");
